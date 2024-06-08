b3577a263c7887e8887135927db92ab9add486ab
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-06-08 21:09:54 +0800

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -457,7 +457,7 @@ class Controls extends FlxActionSet
 		inline forEachBound(Control.NOTE_DOWN, (action, state) -> addButtonNOTES(action, Hitbox.buttonDown, state));
 		inline forEachBound(Control.NOTE_LEFT, (action, state) -> addButtonNOTES(action, Hitbox.buttonLeft, state));
 		inline forEachBound(Control.NOTE_RIGHT, (action, state) -> addButtonNOTES(action, Hitbox.buttonRight, state));
-		inline forEachBound(Control.SPACE, (action, state) -> addbuttonUI(action, Hitbox.buttonSpace, state));
+		inline forEachBound(Control.SPACE, (action, state) -> addbuttonuUI(action, Hitbox.buttonSpace, state));
 	}
 	
 	public function setVirtualPadUI(virtualPad:FlxVirtualPad, ?DPad:FlxDPadMode, ?Action:FlxActionMode) 
