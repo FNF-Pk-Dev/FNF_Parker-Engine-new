b9126e1c8f64a50c975e82c1f171b255a4f4a8fd
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-06-19 10:23:02 +0800

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -21,7 +21,7 @@ import android.AndroidControls.AndroidControls;
 import android.FlxVirtualPad;
 #end
 
-class MusicBeatState extends modcharting.ModchartMusicBeatState
+class MusicBeatState extends FlxUIState
 {
 	private var curSection:Int = 0;
 	private var stepsToDo:Int = 0;
