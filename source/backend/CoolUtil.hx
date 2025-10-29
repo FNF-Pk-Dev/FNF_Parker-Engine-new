806b8bc3e736d3f08ecdade6eea55f735bfb6331
AjwwkD <107187521+Ajwyunsx@users.noreply.github.com>
2025-10-29 18:02:54 +0800

    Update CoolUtil.hx

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -249,6 +249,16 @@ class CoolUtil
 			+ '/'
 			+ FlxSave.validate(FlxG.stage.application.meta.get('file')) #end;
 	}
+	inline public static function colorFromString(color:String):FlxColor
+	{
+		var hideChars = ~/[\t\n\r]/;
+		var color:String = hideChars.split(color).join('').trim();
+		if(color.startsWith('0x')) color = color.substring(color.length - 6);
+
+		var colorNum:Null<FlxColor> = FlxColor.fromString(color);
+		if(colorNum == null) colorNum = FlxColor.fromString('#$color');
+		return colorNum != null ? colorNum : FlxColor.WHITE;
+	}
 	public static function showPopUp(message:String, title:String):Void
 	{
 		#if android
